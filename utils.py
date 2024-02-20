62e0648986e2e2a5584e2732f7b07cfef95f03ec
Mahdi Rafati <mahdirafati680@gmail.com>
2024-02-20 16:41:42 +0330

    utils created

---
+++ utils.py
@@ -0,0 +1,12 @@
+folder_file_maps = {
+    'Audios': (".3ga", ".aac", ".ac3", ".aif", ".aiff",
+              ".alac", ".amr", ".ape", ".au", ".dss",
+              ".flac", ".flv", ".m4a", ".m4b", ".m4p",
+              ".mp3", ".mpga", ".ogg", ".oga", ".mogg",
+              ".opus", ".qcp", ".tta", ".voc", ".wav",
+              ".wma", ".wv"),
+    'Videos': (".webm", ".MTS", ".M2TS", ".TS", ".mov",
+               ".mp4", ".m4p", ".m4v", ".mxf"),
+    'Images': (".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png",
+               ".gif", ".webp", ".svg", ".apng", ".avif"),
+}
