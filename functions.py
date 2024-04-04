ff5140d68a3fe87193ab272c4682f5860538b03b
Mahdi Rafati <mahdirafati680@gmail.com>
2024-04-04 13:23:14 +0330

    main function created - organaize function added to functions

--- functions.py
+++ functions.py
@@ -4,14 +4,44 @@ import shutil
 from utils import folder_file_maps
 
 
-def get_file_type(file: str) -> str: #! New
+def organize():
+    """Organize existing files on the given path."""
+
+    # Take target path and change the current directory
+    change_to_terget_path()
+    # Get the available files
+    files = [fl for fl in os.listdir() if os.path.isfile(fl)]
+    # Create folders and move files
+    for file in files:
+        try:
+            # Finding the corresponding folder name to the file extension
+            folder = get_file_type(file)
+        except Exception as err:
+            print(err)
+            print("Add the extinsion along with the related folder name using option-2, then reorganize.")
+            continue
+        # Moving the file into related folder
+        transfer(folder, file)
+
+def change_to_terget_path() -> None:
+    """Get target location from user and change directory to the given path."""
+
+    while True:
+        try:
+            path = input("Enter the target path for organizing: ").strip() 
+            os.chdir(path)
+            break
+        except FileNotFoundError:
+            print("This path doesn't exist!")
+
+def get_file_type(file: str) -> str: 
     """Return corresponding folder name to the file."""
     
     file_extension = Path(file).suffix
     for key in folder_file_maps:
         if file_extension in folder_file_maps[key]:
             return key
-    raise Exception("This type of file is not supported!")
+    raise Exception(f"'{file_extension}' files are not supported!")
 
 def transfer(folder_name: str, file: str) -> None:
     """Create a folder and transfer the file into it."""
@@ -25,7 +55,7 @@ def transfer(folder_name: str, file: str) -> None:
         # Remove the file if it already exists
         os.remove(file)
 
-def add_new_item(folder_name: str, file_ext: str) -> None: #! New
+def add_new_item(folder_name: str, file_ext: str) -> None:
     """Add new extension to the given folder name."""
     
     folder_file_maps[folder_name].add(file_ext)
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-import os 
-from functions import get_file_type, transfer, add_new_item
+import time
+from functions import organize, add_new_item
 
-# Change current directory to the target directory
-try:
-    print("Welcome to the program :)")
-    # Get the target location
-    target_directory = input("please enter the target location for organizing: ").strip() #"/home/mehdirt/Downloads" 
-    os.chdir(target_directory)
+#TODO: Make the printing better
 
-except FileNotFoundError:
-    print("The Path doesn't exist!")
+def main() -> None:
 
-# Getting the available files
-files = [fl for fl in os.listdir() if os.path.isfile(fl)]
+    print("""Welcome to File Organizer 🔍\nOptions:
+    1- Organize a Folder
+    2- Add New Extension
+    3- exit""")
+    while True:
+        option = int(input("Choose an option to be performd: "))
+        if option == 1:
+            organize()
+        elif option == 2:
+            add_new_item() #FIXME
+        elif option == 3:
+            print("Good Bye 👋")
+            time.sleep(1.25)
+            break
 
-# Create folders and move files
-for file in files:
-    try:
-        # Finding the corresponding folder name to the file extension
-        folder = get_file_type(file)
-
-    except Exception as err:
-        print(err)
-        print("You can add new extension along with a folder.")
-        #TODO: Add this function -> add_new_item()
-    # Moving the file into related folder
-    transfer(folder, file)
-
-# print(os.path.join('usr', 'bin', 'spam'))
-# print(Path('usr').joinpath('bin').joinpath('spam'))
+if __name__ == '__main__':
+    main()
